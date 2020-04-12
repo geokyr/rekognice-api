f374ab71e52960314de07cdddf0dfeb1e78a8cc8
George Kyriakopoulos <giwirawxzkyr@gmail.com>
2020-04-13 01:57:41 +0300

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-cubed-18793',
     user : 'postgres',
     password : 'postgres',
     database : 'rekognice'
