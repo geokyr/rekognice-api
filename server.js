545e8eb2cf059fbe175639c3849e8760a6d9e667
George Kyriakopoulos <giwirawxzkyr@gmail.com>
2020-04-13 03:07:14 +0300

    updated server

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//app.get('/', (req, res) => {res.send(database.users)})
-app.get('/', (req, res) => {res.send('It is working!')})
+app.get('/', (req, res) => {res.send(db.users)})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.get('/profile/:id', profile.handleProfileGet(db))
